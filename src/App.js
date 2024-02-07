a4a8ee626ad8381e6fc540dc0cd80d5049eeae58
ArvickC <arvick@outlook.com>
2024-02-07 15:22:07 -0800

    Archive and Upload Test-Site

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Pack of Parts Test Page
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
